a37e2b74f2e6e589070035632ccff0b09c637984
Kamal Nayan <geniusamansingh@gmail.com>
2023-01-01 10:39:39 +0000

    license and pkg files added

---
+++ index.js
@@ -0,0 +1,56 @@
+require('dotenv').config()
+const axios = require('axios')
+const TelegramBot = require('node-telegram-bot-api')
+
+// Modify the API_ID BOT_TOKEN and X_API_KEY in .env file
+const API_ID = process.env.API_ID // Zettablock API ID
+const BOT_TOKEN = process.env.BOT_TOKEN // Your Telegram Bot Token
+const X_API_KEY = process.env.X_API_KEY // Your Zettablock API Key
+
+const bot = new TelegramBot(BOT_TOKEN, { polling: true }) // Create a bot that uses 'polling' to fetch new updates
+
+// Get token data from Zettablock API
+async function queryToken(symbol) {
+	var result
+	const options = {
+		method: 'POST',
+		url: `https://api.zettablock.com/api/v1/dataset/${API_ID}/graphql`, // Zettablock API endpoint
+		headers: {
+			accept: 'application/json',
+			'X-API-KEY': X_API_KEY, // Your ZettaBlock API Key
+			'content-type': 'application/json',
+		},
+		data: {
+			// GraphQL query starts here, token symbol is passed as a variable
+			query: `
+				{
+					records(symbol: "${symbol}", limit: 1 )
+					{
+						name
+						symbol
+						address
+						supply
+						type
+						token_authority
+					}
+				}
+			`,
+			// GraphQL query ends here
+		},
+	}
+
+	await axios
+		.request(options) // Send the request to Zettablock API
+		.then(function (response) {
+			// If the request is successful, return the data
+			// console.log(response.data.data.records)
+			result = response.data.data.records
+		})
+		.catch(function (error) {
+			// If the request is failed, return the error
+			console.error(error)
+		})
+	// return the result array
+	return result
+}
+
